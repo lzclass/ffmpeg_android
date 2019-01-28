9adc5bdccea9a50ca5ea76725a35588085878d1e
liuzhao <63993048@qq.com>
2019-01-28 18:17:26 +0800

    1

--- app/src/main/java/com/example/lz/ffmpeg/FFmpegNativeUtil.java
+++ app/src/main/java/com/example/lz/ffmpeg/FFmpegNativeUtil.java
@@ -4,23 +4,33 @@ import android.view.Surface;
 
 public class FFmpegNativeUtil {
 
-        static {
-            System.loadLibrary("avcodec-57");
-            System.loadLibrary("avdevice-57");
-            System.loadLibrary("avfilter-6");
-            System.loadLibrary("avformat-57");
-            System.loadLibrary("avutil-55");
-            System.loadLibrary("postproc-54");
-            System.loadLibrary("swresample-2");
-            System.loadLibrary("swscale-4");
-        }
-        /**
-         * javah -classpath E:\Android\Sdk\platforms\android-28\android.jar;. main.java.com.example.lz.ffmpeg.FFmpegNativeUtil
-         * 播放视频流
-         * @param videoPath（本地）视频文件路径
-         * @param surface
-         */
-        public native void videoStreamPlay(String videoPath, Surface surface);
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("avcodec-57");
+        System.loadLibrary("avdevice-57");
+        System.loadLibrary("avfilter-6");
+        System.loadLibrary("avformat-57");
+        System.loadLibrary("avutil-55");
+        System.loadLibrary("postproc-54");
+        System.loadLibrary("swresample-2");
+        System.loadLibrary("swscale-4");
+    }
 
+//    /**
+//     * javah -classpath E:\Android\Sdk\platforms\android-28\android.jar;. main.java.com.example.lz.ffmpeg.FFmpegNativeUtil
+//     * 播放视频流
+//     *
+//     * @param videoPath（本地）视频文件路径
+//     * @param surface
+//     */
+//    public native void videoStreamPlay(String videoPath, Surface surface);
+
+    private static native int run(int cmdLen, String[] cmd);
+
+    public static native String test();
+
+    public static int run(String[] cmd) {
+        return run(cmd.length, cmd);
+    }
 
 }
--- app/src/main/java/com/example/lz/ffmpeg/MainActivity.java
+++ app/src/main/java/com/example/lz/ffmpeg/MainActivity.java
@@ -1,29 +1,61 @@
 package com.example.lz.ffmpeg;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
-   MyVideoView my_video;
-    static {
-        System.loadLibrary("native-lib");
-    }
+    MyVideoView my_video;
+    TextView sample_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        my_video = findViewById(R.id.my_video);
-        tv.setText(stringFromJNI());
+        sample_text = findViewById(R.id.sample_text);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    100);
+        }
+        ffmpegTest();
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private static Handler handler = new Handler();
 
+    private void ffmpegTest() {
+        new Thread() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                String input = Environment.getExternalStorageDirectory().getPath() + "/DCIM/Video/V80930-085944.mp4";
+                String output = Environment.getExternalStorageDirectory().getPath() + "/DCIM/Video/a_output.mp4";
+                //剪切视频从00：10-00：28的片段
+                String cmd = "ffmpeg -d -ss 00:00:10 -t 00:00:28 -i %s -vcodec copy -acodec copy %s";
+                cmd = String.format(cmd, input, output);
+                FFmpegNativeUtil.run(cmd.split(" "));
+                final String logString = "FFmpegTest_run: 耗时：" + (System.currentTimeMillis() - startTime);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        sample_text.setText(logString);
+                    }
+                });
+                Log.d("FFmpegTest", "run: 耗时：" + (System.currentTimeMillis() - startTime));
+            }
+        }.start();
+
+    }
+
+    public native String stringFromJNI();
 }
